9c79e7e677eddcf4759ae321b08d17545efe0965
colinbinion <colin.binion@me.com>
2017-01-10 10:26:03 -0700

    chapter2TS update

--- chapter2Sandbox/main.ts
+++ chapter2Sandbox/main.ts
@@ -11,21 +11,27 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
   // template property defines the HTML markup for rendering this component
   template: '<h1>Hello {{ name }}!</h1>'
 })
+// annotated HelloWorldComponent class represents the component
 class HelloWorldComponent {
+  // name property is used in the data-binding expression on the components template
   name: string;
 
+// inside constructor initialize the name property of the value angular2 bound to the template
   constructor() {
     this.name = 'Angular';
   }
 }
 
 // Module
+// declares content of module
 @NgModule({
   imports:      [ BrowserModule ],
   declarations: [ HelloWorldComponent ],
   bootstrap:    [ HelloWorldComponent ]
 })
+// declares class representing module
 export class AppModule { }
 
 // App bootstrap
+// loads module
 platformBrowserDynamic().bootstrapModule(AppModule);
